0f667166f828b0904c1bd950dc5cb6d5eb4a608d
EgleNavickiene <navickiene@gmail.com>
2021-07-08 18:05:33 +0300

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,291 @@
+//Kintamuju skaiciavimas
+
+var skaiciavimas;
+
+skaiciavimas = 1;
+
+skaiciavimas += 2;
+
+skaiciavimas -= 2;
+
+skaiciavimas = skaiciavimas * 3;
+
+skaiciavimas = skaiciavimas / 3;
+
+//alert(skaiciavimas);
+
+var petras = [
+    0 , 
+    15,
+    2 , 
+    3 , 
+];
+
+//console.log( petras[1] );
+
+//var = variable (angl.) kintamasis 
+
+//Eventas skirtas issaukti reakcija paspaudus mygtuka
+var pasisveikinimas = 'Labas lietuva!';
+
+
+
+
+
+
+
+document.getElementById('kvietejas').addEventListener("click", ikrepseli);
+
+function ikrepseli() {
+
+    var kiekis = document.getElementById('kiekis').value;
+    var zodis  = 'prekes';
+
+    if(kiekis%10 == 1) {
+        zodis  = 'prekę';
+    }
+
+    if( (kiekis > 10 && kiekis < 20) || kiekis%10 == 0) {
+        zodis = 'prekių';
+    } 
+
+    var zinute = 'Jūs sėkmingai pridėjote ' + kiekis + ' ' + zodis + '  į savo krepšelį!';
+
+    if(kiekis > 100) {
+         zinute = 'Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas.';
+         //classList.add('error');//
+    }
+
+    if(kiekis < 1) {
+         zinute = 'Pasirinktas per mažas kiekis.';
+         //classList.add('error');//
+    }
+
+    document.getElementById('messages_order').innerHTML = ''+ zinute;
+    
+};
+
+
+
+document.getElementById('reset').addEventListener("click", atnaujinti);
+
+function atnaujinti() {
+
+    location.reload();
+
+}
+
+var i = 0;
+
+while(i < 10) {
+
+    //console.log(i);
+    i++;
+
+}
+
+i = 0;
+
+do {
+    //console.log('Skaicius yra: ' + i);
+    i++;
+}
+while (i < 10)
+
+
+var petras = [
+    5 , 
+    6, 
+    7 , 
+    8 , 
+    9
+];
+
+//console.log(petras[0]);
+
+/*for(let i = 0; i < petras.length; i++) {
+    console.log('Masyvo petras indeksas: ' + petras[i]);
+}*/
+
+const duomenys = [
+    {
+        miestas: "Kaunas",
+        adresas: "Jonavos 56",
+        data: "2020-07-01",
+        kiekis: "10"
+    },
+    {
+        miestas: "Vilnius",
+        adresas: "Gelezinio vilko 20",
+        data: "2020-07-01",
+        kiekis: "15"
+    },
+    {
+        miestas: "Klaipeda",
+        adresas: "Ligonines 20",
+        data: "2020-07-01",
+        kiekis: "25"
+    }
+];
+
+
+let html = '';
+let ending = '';
+
+for(let indeksas in duomenys) {
+
+    html += '<tr>'; 
+
+    for(let indeksas2 in duomenys[indeksas]) {
+        
+        html += '<td>' + duomenys[indeksas][indeksas2] + '</td>';
+
+    }
+
+    html += '</tr>';
+
+    //----------jQuery test
+
+  /*jQuery(document).ready(function() {
+ 
+        jQuery(duomenys).each(function(indeksas, reiksme){
+       
+          jQuery(reiksme).each(function(indeksas, reiksme) {
+            console.log(reiksme['miestas']);
+          });
+          
+        });
+       
+      });*/
+
+//--------------
+
+    //console.log(eilute);
+
+}
+
+document.getElementById('lentelesVidus').innerHTML = html;
+
+
+
+function testineFunkcija() {
+    //alert('ko reikia');
+}
+
+let reiksme = 0;
+
+switch(reiksme) {
+
+    case 0:
+        testineFunkcija();
+    break;
+
+    case 1:
+        alert('Reiksme yra 1');
+    break;
+
+    default:
+        alert('Reiksmes nera');
+
+}
+
+document.getElementById('rodyti').onclick = function() {
+
+    var lentele = document.getElementById('lentele');
+    
+    lentele.classList.toggle('show'); 
+
+    if( lentele.classList.contains('show') == false ) {
+        document.getElementById('rodyti').innerText = 'Rodyti lentelę';
+    } else {
+        document.getElementById('rodyti').innerText = 'Slėpti lentelę';
+    }
+
+};
+
+document.getElementById('skaiciuoti').onclick = function() {
+
+    var skaiciuokles = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+    ];
+    
+    var total = 0;
+    
+    for(let i = 0; i < skaiciuokles.length; i++) {
+        total += skaiciuokles[i];
+    }
+
+    document.getElementById('total').innerHTML = total;
+
+};
+
+// pasirinkimas
+document.getElementById('submit').onclick = function() {
+
+    var el = document.getElementById('pasirinkimas');
+
+    var tekstas = '';
+
+    switch(el.value) {
+
+        case 'volvo':
+            tekstas = 'Daug masės, daug plieno.';
+            break;
+
+        case 'saab':
+            tekstas = 'Švedų mašina';
+            break;
+
+        case 'opel':
+            tekstas = 'Stovėdamas rūdyja';
+            break;
+
+        case 'audi':
+            tekstas = 'Audi nečiaudi';
+            break;
+
+        default:
+            tekstas = 'Velniop tą mašiną';
+
+    }
+
+    //document.getElementById('atsakymoZinute').innerText = tekstas;
+
+    jQuery('.atsakymoZinute').html(tekstas)
+}
+
+
+jQuery(document).ready(function() {  
+
+   jQuery('.naujasmygtukas').click(function(){
+
+        jQuery('#naujasmygtukas').addClass('klase');
+
+        jQuery('#atsakymoZinute').addClass('klase');
+
+   });
+
+
+    jQuery('#kitasmygtukas').click(function(){
+
+        jQuery(".naujasmygtukas, .atsakymoZinute").removeClass('klase');
+
+    }); 
+});
+
+//---Integruotas---------------
+
+$('.integruotaNuoroda').click(function() {
+    alert('Paspaudimas');
+});
+
+//----w3schools pvz.----------------
+$(document).ready(function(){
+    $("p").click(function(){
+      $(this).hide();
+    });
+  });
+
+
+
+
