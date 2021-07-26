8ce9cbc3065fc58c62fdf847019f4f70a437a1c9
EgleNavickiene <navickiene@gmail.com>
2021-07-27 00:36:07 +0300

    String Methods

--- assets/js/newfile.js
+++ assets/js/newfile.js
@@ -268,7 +268,7 @@ document.getElementById("nugaletojas").innerHTML = string;
 let kalimas = 1;
 let smugis = 1;
 
-for (let i = 1; i < 6; i++) {
+for (let i = 1; i < 50; i++) {
   for(let k = randomSkaicius(5, 20); k<85; k+=randomSkaicius(5, 20)){
     smugis++;
     document.getElementById("kiek-smugiu").innerHTML = "smugiai " + i;
@@ -277,19 +277,192 @@ for (let i = 1; i < 6; i++) {
 
 }
 
-
-
 // Raudoni kvadratai
 
+document.querySelector('.appended-text').innerHTML += '<div class="septinta_uzduotis"></div>';
+
+document.querySelector('.septinta_uzduotis').innerHTML += '<h3>Septinta užduotis</h3>';
+
+let el_septinta = document.querySelector('.septinta_uzduotis');
+
+el_septinta.innerHTML += '<div class="konteinerisTetis"></div>';
+
+let el_tevinis = document.querySelector('.konteinerisTetis');
 
 
+let tevinio_aukstis = el_tevinis.offsetHeight;
+let tevinio_plotis  = el_tevinis.offsetWidth;
+let nepataike         = 0;
 
+i = 0;
 
-  
+while (i < 100) {
+
+    el_tevinis.innerHTML += '<div class="mazasKvadratelis"></div>';
     
+    let elementas = document.querySelectorAll('.mazasKvadratelis')[i];
+    let atsitiktinis_virsus = randomSkaicius(0, 550);
+    let atsitiktine_kaire   = randomSkaicius(0, 550);
+
+    elementas.style.top = atsitiktinis_virsus+'px';
+    elementas.style.left = atsitiktine_kaire+'px';
+
+    if((atsitiktinis_virsus > tevinio_aukstis) 
+        || (atsitiktine_kaire > tevinio_plotis))
+        nepataike++;
+
+    i++;
+}
+
+el_tevinis.innerHTML += '<div style="color: darkgreen; font-size: 100px; position: absolute; top: 200px; left: 200px; font-weight: bold; text-shadow: -2px -2px #ffffff, -2px 1px #ffffff, 1px -2px #ffffff, 1px 1px #ffffff">' + nepataike + '</div>';
+el_septinta.innerHTML += '<h4 style="margin-top: 100px;">Nepataikyta kartų: ' + nepataike +  '</h4>';
 
 
+//2021 - 07 - 26 d.
+
+let stringas = 'Pavasarį žydi labai daug medžių';
+//apie length
+// console.log(stringas.length);
+
+//apie split
+// console.log(stringas.split(" "));
+
+//apie replace
+// console.log(stringas.replace('žydi', 'atgimsta')); //1 zodis - ieskomas, 2 zodis - kuo keiciame
+
+//apie repeat
+// console.log(stringas.repeat(2));
+
+//apie charAt
+// console.log(stringas.charAt(10));
+
+//apie substract
+//console.log(stringas.substr(9, 5)); // - nuo 9 pozicijos rodys 5 simbolius!
+
+//apie toUppercase
+// console.log(stringas.toUpperCase());
+
+//apie trim
+// stringas = '   Pavasarį žydi labai daug medžių    ';
+//console.log(stringas.trim());
+
+//apie startsWith 
+//boolean: true arba falce
+// console.log(stringas.startsWith('Pavasarį'));
+
+//apie endsWith
+// console.log(stringas.endsWith('ų'));
+
+//apie includes
+// console.log(stringas.includes('medžių'));
+
+//apie indexOf
+// console.log(stringas.indexOf('ž'));
+
+//apie lastIndexOf
+// console.log(stringas.lastIndexOf('ž'));
+
+//apie split
+//let stringo_masyvas = stringas.split(" ");
+
+//console.log(stringo_masyvas[4]);  // duoda 5 zodi(nes nuo 0)
+
+// 1 uzd. (aktorius)
+
+document.querySelector('.appended-text').innerHTML += '<br><hr><div class="pirma-uzduotis"><h3>2021 - 07 - 26</h3><hr></div>';
+
+document.querySelector('.pirma-uzduotis').innerHTML += '<h4 style="color: green;">1 užduotis</h4>';
+
+let aktorius = document.querySelector('.pirma-uzduotis');
 
 
+let aktVardas = 'Kristina';
+let aktPavarde = 'Kazlauskaitė';
+if(aktVardas.length > aktPavarde.length){
+  aktorius.innerHTML += `<h4>${aktVardas}</h4>`;
+} else {
+  aktorius.innerHTML += `<h4>${aktPavarde}</h4>`;
+}
 
+// 2 uzd. 
+document.querySelector('.appended-text').innerHTML += '<br><div class="antra-uzduotis"></div>';
+document.querySelector('.antra-uzduotis').innerHTML += '<h4 style="color: green;">2 užduotis</h4>';
+
+let VARDAS = aktVardas.toUpperCase()
+document.querySelector('.antra-uzduotis').innerHTML += `<h4>${VARDAS} ${aktPavarde}</h4>`;
+
+// 3 uzd. 
+document.querySelector('.appended-text').innerHTML += '<br><div class="trecia-uzduotis"></div>';
+document.querySelector('.trecia-uzduotis').innerHTML += '<h4 style="color: green;">3 užduotis</h4>';
+
+let V_1r = aktVardas.substr(0, 1) + '. ';
+let Pav_1r = aktPavarde.substr(0, 1) + '.';
+
+document.querySelector('.trecia-uzduotis').innerHTML += `<div class="h4">${V_1r}${Pav_1r}</h4>`;
+
+// 4 uzd. 
+document.querySelector('.appended-text').innerHTML += '<br><div class="ketvirta-uzduotis"></div>';
+document.querySelector('.ketvirta-uzduotis').innerHTML += '<h4 style="color: green;">4 užduotis</h4>';
+
+let vardo_3pask = aktVardas.slice(Math.max(aktVardas.length -3, 0));
+//console.log(vardo_3pask);
+
+let pav_3pask = aktPavarde.slice(Math.max(aktPavarde.length -3, 0));
+//console.log(Pav_3pask);
+
+document.querySelector('.ketvirta-uzduotis').innerHTML += '<h4>'+vardo_3pask + pav_3pask +'</h4>' ;
+
+// 5 uzd. 
+document.querySelector('.appended-text').innerHTML += '<br><div class="penkta-uzduotis"></div>';
+document.querySelector('.penkta-uzduotis').innerHTML += '<h4 style="color: green;">5 užduotis</h4>';
+
+let strH = 'Once upon a time in hollywood';
+
+let strH_1repl = strH.replace(/o/g,'*');
+
+document.querySelector('.penkta-uzduotis').innerHTML += '<h4 class="H_be_O">'+strH_1repl.replace('O', '*')+'</h4>';
+
+// 6 uzd. 
+document.querySelector('.appended-text').innerHTML += '<br><div class="sesta-uzduotis"></div>';
+document.querySelector('.sesta-uzduotis').innerHTML += '<h4 style="color: green;">6 užduotis</h4>';
+
+//                let str_be_o = document.querySelector('.H_be_O');
+
+var num_matches = (strH.match(/o/gi).length);
+
+document.querySelector('.sesta-uzduotis').innerHTML += '<h4>'+'Masyve "'+ strH + '" yra '+ num_matches + " \"o\" raidės."+ '</h4>' ;
+
+document.querySelector('.appended-text').innerHTML += '<br><div class="septinta-uzduotis"></div>';
+document.querySelector('.septinta-uzduotis').innerHTML += '<h4 style="color: green;">7 užduotis</h4>';
+
+let str3 = 'An American in Paris';
+let str4 = 'Breakfast at Tiffany\'s';
+let str5 = '2001: A Space Odyssey';
+let str6 = 'It\'s a Wonderful Life'
+console.log(str4);
+
+sliced3 = str3.replace(/[aeiouy]/ig,'');
+sliced4 = str4.replace(/[aeiouy]/ig,'');
+sliced5 = str5.replace(/[aeiouy]/ig,'');
+sliced6 = str6.replace(/[aeiouy]/ig,'');
+
+document.querySelector('.septinta-uzduotis').innerHTML += '<h4>'+str3+ '</br>' + sliced3 + '</h4>';
+document.querySelector('.septinta-uzduotis').innerHTML += '<h4>' + str4+ '</br>' + sliced4 + '</h4>';
+document.querySelector('.septinta-uzduotis').innerHTML += '<h4>' + str5+ '</br>' + sliced5 + '</h4>';
+document.querySelector('.septinta-uzduotis').innerHTML += '<h4>' + str6+ '</br>' + sliced6 + '</h4>';
+
+document.querySelector('.appended-text').innerHTML += '<br><div class="astunta-uzduotis"></div>';
+document.querySelector('.astunta-uzduotis').innerHTML += '<h4 style="color: green;">8 užduotis</h4>';
+
+let str8 =  "Don't Be a Menace to South Central While Drinking Your Juice in the Hood";
+
+let str8_masyvas = str8.split(" ");
+
+console.log(str8_masyvas);
+
+let str8b = "Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale";
+
+let str8b_masyvas = str8b.split(" ");
+
+console.log(str8b_masyvas);
 
